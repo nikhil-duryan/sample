c68620468141b522ba1363ebc15f09e6dbfe756a
Nikhil Kumar <nikhil.kumar@314ecorp.com>
2025-03-07 16:31:07 +0530

    add func to print sum

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    a = input("Enter a number: ")
+    b = input("Enter another number: ")
+
+    total = int(a) + int(b)
+
+    print("The sum of the two numbers is: ", total)
+
+
+if __name__ == "__main__":
+    main()
